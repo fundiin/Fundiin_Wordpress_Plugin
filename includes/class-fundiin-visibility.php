720fea1390daafaa5cdc3740f29db6d20688b542
baonguyentq <quocbao.nguyentran@outlook.com>
2025-04-01 16:21:41 +0700

    add cartItems to checkout page script

--- includes/class-fundiin-visibility.php
+++ includes/class-fundiin-visibility.php
@@ -7,13 +7,11 @@ class Fundiin_Visibility
 {
     private $action_registered = false;
 
-    function __construct()
-    {
+    function __construct() {
         $this->register_action();
     }
 
-    function register_action()
-    {
+    function register_action() {
         if (!$this->action_registered) {
             $this->add_to_single_product();
             $this->action_registered = true;
@@ -22,18 +20,15 @@ class Fundiin_Visibility
         $this->add_to_cart_page();
     }
 
-    public function add_to_single_product()
-    {
+    public function add_to_single_product() {
         add_action('woocommerce_before_add_to_cart_form', array($this, 'fundiin_price_in_product_single'), 13);
     }
 
-    public function add_to_cart_page()
-    {
+    public function add_to_cart_page() {
         add_action('woocommerce_proceed_to_checkout', array($this, 'fundiin_price_in_cart_page'), 20);
     }
 
-    public function fundiin_price_in_product_single()
-    {
+    public function fundiin_price_in_product_single() {
         global $product;
 
         if ($product) {
@@ -42,59 +37,61 @@ class Fundiin_Visibility
             $host = fundiin()->settings->get_fundiin_host();
 
             echo '<div id="script-general-container"></div>';
-            echo '<script type="text/javascript">var fundiinDetailConfig = { data: { amount: ' . $product_price . ' }, style: {} }; </script>';
+            echo '
+                <script type="text/javascript">
+                    var fundiinDetailConfig = {
+                        data: { amount: ' . $product_price . ' },
+                        style: {},
+                    };
+                </script>
+            ';
             echo '<script type="application/javascript" async src="' . $host . '/merchants/productdetailjs/' . $merchantId . '.js"></script>';
         }
     }
 
-    public function fundiin_price_in_cart_page()
-    {
+    public function fundiin_price_in_cart_page() {
         $cart_price = WC()->cart->total;
 
         if ($cart_price) {
             $merchantId = fundiin()->settings->merchantId;
             $host = fundiin()->settings->get_fundiin_host();
             echo '<div id="script-general-container"></div>';
-            echo '<script type="text/javascript">var fundiinCartConfig = { data: { amount: ' . $cart_price . ' }, style: {}; </script>';
+            echo '
+                <script type="text/javascript">
+                    var fundiinCartConfig = {
+                        data: { amount: ' . $cart_price . ' },
+                        style: {},
+                    };
+                </script>
+            ';
             echo '<script type="application/javascript" async src="' . $host . '/merchants/cartjs/' . $merchantId . '.js"></script>';
         }
     }
 
-    public function fundiin_in_checkout()
-    {
+    public function fundiin_in_checkout() {
         $merchantId = fundiin()->settings->merchantId;
         $host = fundiin()->settings->get_fundiin_host();
+        $cart_items = [];
+
+        foreach (WC()->cart->cart_contents as $cart_item) {
+            $cart_items[] = [
+                'id' => $cart_item['data']->id,
+                'name' => $cart_item['data']->name,
+                'price' => $cart_item['data']->price,
+                'regularPrice' => $cart_item['data']->regular_price,
+                'slug' => $cart_item['data']->slug,
+                'sku' => $cart_item['data']->sku,
+            ];
+        }
 
         echo '<div id="script-checkout-container"></div>';
-        echo '<script type="application/javascript" src="' . $host . '/merchants/checkoutjs/' . $merchantId . '.js"></script>';
-        echo "
-            <script type='text/javascript'>
-                var trackingScript = document.createElement('script');
-                trackingScript.type = 'text/javascript';
-                trackingScript.innerHTML = `
-                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                    })(window,document,'script','dataLayer','GTM-TP47LP6K');
-                `;
-                document.head.appendChild(trackingScript);
-
-                var eventScript = document.createElement('script');
-                eventScript.type = 'text/javascript';
-                eventScript.innerHTML = `
-                    window.dataLayer = window.dataLayer || [];
-                    window.addEventListener('DOMContentLoaded', function() {
-                        window.dataLayer.push({
-                            event: 'view_checkout',
-                            event_category: 'mx_site',
-                            event_action: 'view',
-                            event_label: 'mx_checkout',
-                        });
-                    });
-                `;
-                document.body.appendChild(eventScript);
+        echo '
+            <script type="text/javascript">
+                var fundiinCheckoutConfig = {
+                    data: { cartItems: ' . $cart_items . ' },
+                };
             </script>
-        ";
+        ';
+        echo '<script type="application/javascript" src="' . $host . '/merchants/checkoutjs/' . $merchantId . '.js"></script>';
     }
 }
